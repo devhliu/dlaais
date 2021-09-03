ff95eaeebe79aa5e760f0db0598ce3d0363e05f2
devhliu <huiliu.liu@gmail.com>
2021-09-03 12:22:56 +0800

    add ants 4d supporting

--- dlaais/ants/registration_antspy.py
+++ dlaais/ants/registration_antspy.py
@@ -29,6 +29,10 @@ def rigid_reg_3d_nifti_groups(series_root, ref_file, in_filenames, out_filenames
     """
     if not os.path.exists(ref_file): return False
     fi = ants.image_read(ref_file)
+    fi_mask = ants.get_mask(fi)
+    fi = ants.mask_image(fi, fi_mask)
+    ants.image_write(fi_mask, os.path.join(os.path.dirname(series_root), 'head_mask.nii.gz'))
+    ants.image_write(fi, os.path.join(os.path.dirname(series_root), 'CTP_P0.nii.gz'))
     for in_filename, out_filename in zip(in_filenames, out_filenames):
         print('working on %s - %s'%(series_root, in_filename))
         in_file = os.path.join(series_root, in_filename + '.nii.gz')
@@ -37,8 +41,11 @@ def rigid_reg_3d_nifti_groups(series_root, ref_file, in_filenames, out_filenames
         if not os.path.exists(in_file): continue
         if os.path.exists(out_nii_file): continue
         mv = ants.image_read(in_file)
-        mx = ants.registration(fixed=fi, moving=mv, type_of_transform='Rigid')
-        ants.image_write(mx['warpedmovout'], out_nii_file)
+        mv = ants.mask_image(mv, fi_mask)
+        mx = ants.registration(fixed=fi, moving=mv, type_of_transform='Rigid', mask=fi_mask,
+                               reg_iterations=(5, 5, 0), aff_iterations=(20, 10, 10, 10))
+        mv_rigid = mx['warpedmovout']
+        ants.image_write(mv_rigid, out_nii_file)
     return
 
 #--------------------------------------------------------------------------------------------------------
@@ -56,22 +63,23 @@ def rigid_reg_4d_nifti(patient_niix_root, dyn_1min_4d_niifilename, ref_nii_file=
     if os.path.exists(dyn_4d_mc_niifile): return True
     dyn_4d_mc_nii_root = os.path.join(patient_niix_root, dyn_1min_4d_niifilename)
     if os.path.exists(dyn_4d_mc_nii_root): shutil.rmtree(dyn_4d_mc_nii_root)
-    os.makedirs(dyn_4d_mc_nii_root, exist_ok=True)
-    nib_4d_img = nib.load(dyn_4d_niifile)
-    affine = nib_4d_img.affine
-    np_4d_img = nib_4d_img.get_fdata()
-    dtype = np_4d_img.dtype
-    N = np_4d_img.shape[-1]
-    in_filenames = []
-    out_filenames = []
-    for i in range(N):
-        np_3d_img_1min = np_4d_img[:, :, :, i]
-        nib_3d_img_1min = nib.Nifti1Image(np_3d_img_1min, affine=affine)
-        nib.save(nib_3d_img_1min, os.path.join(dyn_4d_mc_nii_root, dyn_1min_4d_niifilename + '_{:04d}.nii.gz'.format(i)))
-        in_filenames.append(dyn_1min_4d_niifilename + '_{:04d}'.format(i))
-        out_filenames.append(dyn_1min_4d_niifilename + '_{:04d}_mc'.format(i))
-    np_4d_img = None
-    nib_4d_img = None
+    if not os.path.exists(dyn_4d_mc_nii_root):
+        os.makedirs(dyn_4d_mc_nii_root, exist_ok=True)
+        nib_4d_img = nib.load(dyn_4d_niifile)
+        affine = nib_4d_img.affine
+        np_4d_img = nib_4d_img.get_fdata()
+        dtype = np_4d_img.dtype
+        N = np_4d_img.shape[-1]
+        in_filenames = []
+        out_filenames = []
+        for i in range(N):
+            np_3d_img_1min = np_4d_img[:, :, :, i]
+            nib_3d_img_1min = nib.Nifti1Image(np_3d_img_1min, affine=affine)
+            nib.save(nib_3d_img_1min, os.path.join(dyn_4d_mc_nii_root, dyn_1min_4d_niifilename + '_{:04d}.nii.gz'.format(i)))
+            in_filenames.append(dyn_1min_4d_niifilename + '_{:04d}'.format(i))
+            out_filenames.append(dyn_1min_4d_niifilename + '_{:04d}_mc'.format(i))
+        np_4d_img = None
+        nib_4d_img = None
 
     # select the reference file
     if ref_nii_file == 'First':
@@ -95,4 +103,51 @@ def rigid_reg_4d_nifti(patient_niix_root, dyn_1min_4d_niifilename, ref_nii_file=
 
     # remove temp files
     if os.path.exists(dyn_4d_mc_nii_root): shutil.rmtree(dyn_4d_mc_nii_root)
+    return True
+
+#--------------------------------------------------------------------------------------------------------
+#
+def apply_mni_tx_4d_mc_nifti(patient_niix_root, dyn_1min_4d_niifilename, mni_txlist, ref_img):
+    """
+    """
+    # decompose 4d nifti file
+    dyn_4d_niifile = os.path.join(patient_niix_root, dyn_1min_4d_niifilename + '.nii.gz')
+    dyn_4d_mni_niifile = os.path.join(patient_niix_root, dyn_1min_4d_niifilename + '_mni.nii.gz')
+    if os.path.exists(dyn_4d_mni_niifile): return True
+    dyn_4d_mni_nii_root = os.path.join(patient_niix_root, dyn_1min_4d_niifilename)
+    if os.path.exists(dyn_4d_mni_nii_root): shutil.rmtree(dyn_4d_mni_nii_root)
+    if not os.path.exists(dyn_4d_mni_nii_root):
+        os.makedirs(dyn_4d_mni_nii_root, exist_ok=True)
+        nib_4d_img = nib.load(dyn_4d_niifile)
+        affine = nib_4d_img.affine
+        np_4d_img = nib_4d_img.get_fdata()
+        dtype = np_4d_img.dtype
+        N = np_4d_img.shape[-1]
+        in_filenames = []
+        out_filenames = []
+        for i in range(N):
+            np_3d_img_1min = np_4d_img[:, :, :, i]
+            nib_3d_img_1min = nib.Nifti1Image(np_3d_img_1min, affine=affine)
+            nib.save(nib_3d_img_1min, os.path.join(dyn_4d_mni_nii_root, dyn_1min_4d_niifilename + '_{:04d}.nii.gz'.format(i)))
+            in_filenames.append(dyn_1min_4d_niifilename + '_{:04d}'.format(i))
+            out_filenames.append(dyn_1min_4d_niifilename + '_{:04d}_mni'.format(i))
+        np_4d_img = None
+        nib_4d_img = None
+
+    # apply transform
+    for in_filename, out_filename in zip(in_filenames, out_filenames):
+        moving = ants.image_read(os.path.join(dyn_4d_mni_nii_root, in_filename + '.nii.gz'))
+        warpedimage = ants.apply_transforms(fixed=ref_img, moving=moving, transformlist=mni_txlist)
+        ants.image_write(warpedimage, os.path.join(os.path.join(dyn_4d_mni_nii_root, out_filename + '.nii.gz')))
+
+    # collect mc nii files
+    nib_imgs = [nib.load(os.path.join(dyn_4d_mni_nii_root, out_filename + '.nii.gz')) for out_filename in out_filenames]
+    nib_4d_img_mc = nib.concat_images(nib_imgs)
+    np_4d_img_mc = nib_4d_img_mc.get_fdata()
+    np_4d_img_mc_new = np_4d_img_mc.astype(dtype)
+    nib_4d_img_mc_new = nib.Nifti1Image(np_4d_img_mc_new, affine=nib_4d_img_mc.affine)
+    nib.save(nib_4d_img_mc_new, dyn_4d_mni_niifile)
+
+    # remove temp files
+    if os.path.exists(dyn_4d_mni_nii_root): shutil.rmtree(dyn_4d_mni_nii_root)
     return True
--- dlaais/ctp/brain_mask.py
+++ dlaais/ctp/brain_mask.py
@@ -10,14 +10,25 @@
 #   2021.08.23
 #--------------------------------------------------------------------------------------------------------
 
+import os
+
 import numpy as np
 import nibabel as nib
 
 #--------------------------------------------------------------------------------------------------------
 #
-def generate_brain_mask(ctp_nii_file, mask_nii_file):
+def generate_brain_mask_from_CTP(ctp_nii_file, phase=0, label='mask', ext='.nii.gz'):
     """
     """
+    # calculate mask nii filename
+    file_root = os.path.dirname(ctp_nii_file)
+    file_basename = os.path.basename(ctp_nii_file)
+    if file_basename.endswith('.nii'): mask_filename = file_basename[:-4] + '_' + label + ext
+    elif file_basename.endswith('.nii.gz'): mask_filename = file_basename[:-7] + '_' + label + ext
+    else: mask_filename = file_basename + '_' + label + ext
+    mask_nii_file = os.path.join(file_root, mask_filename)
+
+    # 
     nib_ctp = nib.load(ctp_nii_file)
     np_ctp = nib_ctp.get_fdata()
     np_max = np.max(np_ctp, axis=-1)
@@ -26,4 +37,25 @@ def generate_brain_mask(ctp_nii_file, mask_nii_file):
     np_mask = np.zeros(np_working.shape, dtype=np.int8)
     np_mask[np_working>10] = 1
     nib_mask = nib.Nifti1Image(np_mask, affine=nib_ctp.affine)
-    nib.save(nib_mask, mask_nii_file)
+    nib.save(nib_mask, )
+#--------------------------------------------------------------------------------------------------------
+#
+def generate_phase_from_CTP(ctp_nii_file, ctp_phase=0, ext='.nii.gz'):
+    """
+    """
+    # calculate ctp phase nii filename
+    label = '{:04d}'.format(ctp_phase) 
+    file_root = os.path.dirname(ctp_nii_file)
+    file_basename = os.path.basename(ctp_nii_file)
+    if file_basename.endswith('.nii'): ctp_phase_filename = file_basename[:-4] + '_' + label + ext
+    elif file_basename.endswith('.nii.gz'): ctp_phase_filename = file_basename[:-7] + '_' + label + ext
+    else: ctp_phase_filename = file_basename + '_' + label + ext
+    ctp_phase_nii_file = os.path.join(file_root, ctp_phase_filename)
+
+    # 
+    nib_ctp = nib.load(ctp_nii_file)
+    np_ctp = nib_ctp.get_fdata()
+    np_phase = np_ctp[:,:,:,0]
+    if ctp_phase < np_ctp.shape[3]: np_phase = np_ctp[:,:,:,ctp_phase]
+    nib_phase = nib.Nifti1Image(np_phase, affine=nib_ctp.affine)
+    nib.save(nib_phase, ctp_phase_nii_file)
